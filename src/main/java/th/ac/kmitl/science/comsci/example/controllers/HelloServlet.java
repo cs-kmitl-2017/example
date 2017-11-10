c45f13902b9660bd798b5359044a760dadf8be3b
PNchee <Peeranat.Chaos@gmail.com>
2017-11-10 12:52:08 +0700

    (Resolved 2) Add 58050350 Peeranat

--- src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/controllers/HelloServlet.java
@@ -23,11 +23,8 @@ public class HelloServlet
         out.println("<ul>");
         out.println("<li>Bearchitect</li>");
         out.println("<li>58050237 Chayapol Limanon</li>");
-<<<<<<< HEAD
-		out.println("<li>58050350 Peeranat Nivesviwat</li>");
-=======
-        out.println("<li>57050294 Pichavarit Panyokaew</li>");
->>>>>>> upstream/master
+        out.println("<li>57050294 Pichavarit Panyokaew</li>")
+        out.println("<li>58050350 Peeranat Nivesviwat</li>");
         out.println("</ul>");
     }
 }

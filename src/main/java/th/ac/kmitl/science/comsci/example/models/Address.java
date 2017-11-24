8230f980091458058dc8a0aecd89bffacfda1098
Nattadet <tooi_321@hotmail.com>
2017-11-24 11:49:52 +0700

    Create Address data class

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Address.java
@@ -0,0 +1,16 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+public class Address {
+    
+    private String Province;
+    //private City city=null;
+    private String Subdistrict;
+    private int PostCode;
+    private String Street;
+    private String Village;
+          
+    public Address(){
+        //city = new City();
+    }
+    
+}

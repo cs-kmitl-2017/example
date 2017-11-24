afbf061fa499d77bfb276c7dd9dbebd3440495b3
Nattadet <tooi_321@hotmail.com>
2017-11-24 12:01:03 +0700

    Modifies naming of varible in Address

--- src/main/java/th/ac/kmitl/science/comsci/example/models/Address.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Address.java
@@ -2,12 +2,12 @@ package th.ac.kmitl.science.comsci.example.models;
 
 public class Address {
     
-    private String Province;
+    private String province;
     //TODO Create variable type of city class
-    private String Subdistrict;
-    private int PostCode;
-    private String Street;
-    private String Village;
+    private String subDistrict;
+    private int postCode;
+    private String street;
+    private String village;
           
     public Address(){
         //TODO put instance of City class to variable

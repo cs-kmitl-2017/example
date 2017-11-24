b9b4e468303aab9f07d6e2f66e70f6e4e416e419
iThanathat <brain12456@hotmail.com>
2017-11-24 11:48:16 +0700

    (Edit)From CityName to City

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/City.java
@@ -0,0 +1,12 @@
+package th.ac.kmitl.science.comsci.example.models;
+public class City {
+    String cityName;
+
+    public City(String cityName){
+        this.cityName = cityName;
+    }
+    public String getName(){
+        return cityName;
+    }
+
+}
---
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/CityTest.java
@@ -0,0 +1,14 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+import junit.framework.Assert;
+import org.junit.Test;
+public class CityTest {
+
+    @Test
+    public void testInputCityName(){
+        String cityName = "Bangkok";
+        City ctyName = new City(cityName);
+        Assert.assertEquals(ctyName.getName(),cityName);
+    }
+
+}

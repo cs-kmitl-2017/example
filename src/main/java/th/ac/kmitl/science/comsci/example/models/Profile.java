25e3f38d437cf142907b1a7c118abed20beed48b
Nattadet <tooi_321@hotmail.com>
2017-11-24 11:35:35 +0700

    Remove comment from abstract class

--- src/main/java/th/ac/kmitl/science/comsci/example/models/Profile.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/Profile.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package th.ac.kmitl.science.comsci.example.models;
 
-/**
- *
- * @author Z5070
- */
 public abstract class Profile {
     private String ID;
     private String Name;

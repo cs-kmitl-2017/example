73f958713a21b0188a35a738f59720713359724a
Nattadet <tooi_321@hotmail.com>
2017-11-24 10:29:34 +0700

    Add new class name BuyerAddress

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/BuyerAddress.java
@@ -0,0 +1,140 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+public class BuyerAddress {
+    
+    private int ID = 123456;
+    
+    private String PostCode;
+    
+    private String BuildingName;
+    
+    private String LineAdress;
+    
+    private String Alley;
+    
+    private String Village;
+    
+    private String Group;
+    
+    private String StreetName;
+    
+    private String CityName;
+    
+    private String CitySubDivisionName;
+    
+    private String CountryID;
+
+    private String CountrySubDivisionID;
+    
+    private String BuildingNumber;
+  
+    
+    public String getPostCode() {
+        return PostCode;
+    }
+  
+    public void setPostCode(String PostCode) {
+        this.PostCode = PostCode;
+    }
+ 
+    public String getBuildingName() {
+        return BuildingName;
+    }
+
+    public void setBuildingName(String BuildingName) {
+        this.BuildingName = BuildingName;
+    }
+    
+    public String getLineAdress() {
+        return LineAdress;
+    }
+
+    public void setLineAdress(String LineAdress) {
+        this.LineAdress = LineAdress;
+    }
+
+    public String getAlley() {
+        return Alley;
+    }
+
+    public void setAlley(String Alley) {
+        this.Alley = Alley;
+    }
+ 
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+       
+    public String getVillage() {
+        return Village;
+    }
+
+    public void setVillage(String Village) {
+        this.Village = Village;
+    }
+
+    public String getGroup() {
+        return Group;
+    }
+
+    public void setGroup(String Group) {
+        this.Group = Group;
+    }
+
+    public String getStreetName() {
+        return StreetName;
+    }
+
+    public void setStreetName(String StreetName) {
+        this.StreetName = StreetName;
+    }
+    
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String CityName) {
+        this.CityName = CityName;
+    }
+    
+    public String getCitySubDivisionName() {
+        return CitySubDivisionName;
+    }
+
+    public void setCitySubDivisionName(String CitySubDivisionName) {
+        this.CitySubDivisionName = CitySubDivisionName;
+    }
+
+    public String getCountryID() {
+        return CountryID;
+    }
+
+    public void setCountryID(String CountryID) {
+        this.CountryID = CountryID;
+    }
+
+    public String getCountrySubDivisionID() {
+        return CountrySubDivisionID;
+    }
+
+    public void setCountrySubDivisionID(String CountrySubDivisionID) {
+        this.CountrySubDivisionID = CountrySubDivisionID;
+    }
+    
+    public String getBuildingNumber() {
+        return BuildingNumber;
+    }
+
+    public void setBuildingNumber(String BuildingNumber) {
+        this.BuildingNumber = BuildingNumber;
+    }
+
+
+    
+    
+
+}
